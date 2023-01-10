5cbe2d9569f282e194c84024b75fa288831c84db
Posashkov AS <alexpos@list.ru>
2023-01-10 15:22:58 +0500

    Step 2. Create base twig layout and layout for index page

--- public/index.php
+++ public/index.php
@@ -1,16 +1,31 @@
 <?php
 
-// Подключение автозагрузки через composer
 require __DIR__ . '/../vendor/autoload.php';
 
+use DI\Container;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
+// Create container
+$container = new Container();
+AppFactory::setContainer($container);
+
+// Set view in Container
+$container->set('view', function () {
+    return Twig::create('../templates'/*, ['cache' => '../cache']*/);
+});
+
+// Create App
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::createFromContainer($app));
+
+// Routes
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Проект Анализатор страниц');
-    return $response;
+    return $this->get('view')->render($response, 'index.twig', []);
 });
 
 $app->run();
